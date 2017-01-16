cc67c50a32d99a19590cc2699a30cb34909aa9c3
Ahmed Latif <ahmedlatif@Ahmeds-MacBook-Pro.local>
2017-01-16 11:37:04 -0500

    Initial Commit

---
+++ AdditionSubtraction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AdditionSubtraction
+//
+//  Created by Ahmed Latif on 1/16/17.
+//  Copyright © 2017 Ahmed Latif. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
